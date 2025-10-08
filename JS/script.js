f0ab9a38e7aa6d555178f17382674bd05ca97d11
P. Nithish <p.nithish564@gmail.com>
2025-10-08 08:23:06 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,8 @@ function greet(name){
     console.log("Hello " + name);
 }
 
-greet("sohan");
+greet("Nithish");
 greet("Rakhi");
+
 
                                                                                                                                                                        
