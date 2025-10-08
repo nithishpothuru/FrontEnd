2361418c98e4a5964f8c0a370d05e1f7e75ebb4e
P. Nithish <p.nithish564@gmail.com>
2025-10-08 08:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// let a = 18;
-// if (a >= 18){
-//     console.log("Adult!");
-// }
-// else{
-//     console.log("Minor!");
-// }
+let a = 18;
+if (a >= 18){
+     console.log("Adult!");
+ }
+ else{
+     console.log("Minor!");
+ }
 
-// for (let i = 1; i <10; i++){
-//     console.log("iteration Number: "+ i);
-// }
+ for (let i = 1; i <10; i++){
+    console.log("iteration Number: "+ i);
+ }
+ function greet(name){
+     console.log("Hello " + name);
+ }
 
-// function greet(name){
-//     console.log("Hello " + name);
-// }
+ greet("Nithish");
+ greet("Rakhi");
 
-// greet("sohan");
-// greet("Rakhi");
 
                                                                                                                                                                        
